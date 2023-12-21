9464dda79665ea987880bf57bafdac6bc1a7c595
Dani Moreira <101526289+danimoreira90@users.noreply.github.com>
2023-12-21 18:16:39 -0300

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 import streamlit as st
 from pages.main import main
-from pages.exp_analysis import exp_analysis
+from pages.exp_analisys import exp_analisys
 from pages.data_quality import data_quality
 from pages.results import results
 
@@ -14,7 +14,7 @@ menu = st.sidebar.selectbox('Selecione uma Página', [
 if menu == 'Principal':
     main.main()
 elif menu == 'Explorando os dados':
-    exp_analysis.exp_analysis()
+    exp_analisys.exp_analisys()
 elif menu == 'Qualidade de dados':
     data_quality.data_quality()
 elif menu == 'Resultado da regressão linear':
